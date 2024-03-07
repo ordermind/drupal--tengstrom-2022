4e44a3c919a0664459357f0e5c04fc2f5d4d9333
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-03-07 09:53:13 +0100

    Add workaround for toolbar padding bug

--- js/tengstrom-2022.js
+++ js/tengstrom-2022.js
@@ -2,14 +2,22 @@
  * @file
  * Tengström 2022 behaviors.
  */
-(function (Drupal) {
+(function ($, Drupal) {
 
   'use strict';
 
   Drupal.behaviors.tengstrom2022 = {
     attach: function (context, settings) {
-      // TODO: Add js behavior
+
+      // Workaround for bug with toolbar module that does not set header padding-top properly on page load which makes
+      // the toolbar cover part of the header
+      $(window).on('load', () => {
+        if ($('#toolbar-item-administration').length) {
+          $('#toolbar-item-administration').click();
+        }
+      })
+
     }
   };
 
-}(Drupal));
+}(jQuery, Drupal));
